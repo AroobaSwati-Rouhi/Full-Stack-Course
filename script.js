f5d793fe223e306027e1706fb4cb82e2014c3208
AroobaSwati-Rouhi <aroobaswati444@gmail.com>
2026-02-25 15:32:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+var a="Arooba";
+var ages= 25;
+    // console.log(a);
+    // console.log(ages);
+    console.log(typeof a);
+    console.log(typeof ages);
+
+// var a="9";
+// a==6
+//     console.log(a);
+
+let selectedItem = null;
+// console.log( selectedItem);
+console.log(typeof selectedItem);
+
+// let isLoggedIn = true;
+// isLoggedIn=false;
+
+// if (isLoggedIn) 
+    
+// {
+//   console.log("Welcome user!");
+// } else {
+//   console.log("Please login first");
+// }console.log(typeof isLoggedIn)
+
+let user="Rouhi";
+let users;
+console.log(typeof user);
+console.log(typeof users);
+
+//    var count = 12;
+//    count =13;
+//    console.log(count+5 )
+
+//    let b = 20;
+// let price = 99.99;
+
+// console.log(b + price);
+
+
+// const salary= 40000;
+// // salary = 40;
+// console.log (salary + 10000);
+
+
+let name = "Arooba";
+let age = 25;
+let isStudent = true;
+
+console.log("My name is " + name + ". I am " + age + " years old. ");
+
+
+let num1 = 10;
+let num2 = 5;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+var C=100;
+F = (C * 9/5) + 32;
+console.log(C);
+console.log((C * 9/5) + 32);
+
+var k=5;
+if (k%2 === 0 )
+{
+  console.log("The number you entered" + k + "Even");
+  
+}else{
+  console.log("The number you entered " + k + " is Odd");
+  
+}
+
+
+var marks=65;
+if (marks > 90)
+{
+  console.log("A");
+  
+}else if(marks > 80)
+  {
+  console.log("B");
+  
+}else if(marks > 70)
+  {
+  console.log("C");
+  
+}else
+    console.log("Fail. Your marks are below 70");
+
+
+   var PersonAge=17;
+if (PersonAge >= 18){
+    console.log("YOUR AGE IS " +  PersonAge + " YOU CAN VOTE");
+
+  // console.log("YOU CAN VOTE");
+  
+}else
+{
+      console.log("YOUR AGE IS "+ PersonAge + " YOU CAN NOT VOTE");
+      // console.log("YOU CAN NOT VOTE");
+}
+
+/*----for loop----*/
+
+for ( i=1; i<=10; i++)
+{
+console.log(i);
+}
+
+
+while (i<10){
+
+  i++;
+}
+let h=10
+do{
+
+console.log(h);
+} 
+while (h<9)
+{
+console.log(h);
+
+}
+
+
+// var j=5;
+// for (i=1; i<=10; i++)
+// {
+// console.log(j + "*" + i + "=" + j*i);
+// }
+
